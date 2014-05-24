c7b148e998f4c1a4514943fbf210253b4c0e79f4
wuzy_oye <oyeswu@gmail.com>
2014-05-24 11:46:46 +0000

    init ghost blog

---
+++ content/themes/casper/assets/js/index.js
@@ -0,0 +1,23 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        // On the home page, move the blog icon inside the header 
+        // for better relative/absolute positioning.
+
+        //$("#blog-logo").prependTo("#site-head-content");
+       hljs.initHighlightingOnLoad();
+
+    });
+  
+    // baidu stat
+   var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+    document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F6f6ed39c29b84e18d888cd882548e531' type='text/javascript'%3E%3C/script%3E"));
+
+}(jQuery));
